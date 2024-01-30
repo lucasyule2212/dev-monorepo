56ad86473e608bd347f767d5161ea13ae06904ba
Lucas Yule <lyrma@cin.ufpe.br>
2024-01-30 01:00:34 -0300

    refactor(ui-tailwind.config): importing shadcn tailwind.config into main ui-tailwind.config.ts

--- packages/ui/tailwind.config.ts
+++ packages/ui/tailwind.config.ts
@@ -1,10 +1,11 @@
 import type { Config } from "tailwindcss";
 import sharedConfig from "@repo/tailwind-config";
+import shadcnConfig from "./tailwind.config";
 
 const config: Pick<Config, "prefix" | "presets" | "content"> = {
   content: ["./src/**/*.tsx"],
   prefix: "ui-",
-  presets: [sharedConfig],
+  presets: [sharedConfig, shadcnConfig],
 };
 
 export default config;

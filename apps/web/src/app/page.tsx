dcd05d73f7f33c18a62a65a3eeea269aa22ed43f
Lucas Yule <lyrma@cin.ufpe.br>
2024-02-05 12:44:38 -0300

    refactor(app-page): adding handleAuth method with session verify + adding Home route and page

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,9 +1,19 @@
-import { Button } from "@repo/ui/components/ui/button";
+import { getSession } from "@auth0/nextjs-auth0";
+import { redirect } from "next/navigation";
+import { NextRequest, NextResponse } from "next/server";
 
-export default function Page() {
-  return (
-    <main>
-      <a href="/api/auth/login">Login</a>
-    </main>
-  );
+export async function handleAuth(req, res) {
+  const session = await getSession(req, res);
+
+  if (!session) {
+    redirect("/api/auth/login");
+  } else {
+    redirect("/home");
+  }
+}
+
+export default async function Page(req: NextRequest, res: NextResponse) {
+  await handleAuth(req, res);
+
+  return <main>Coletando dados...</main>;
 }

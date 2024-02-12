96b0cfd8264f6a9d09ad74faf82e582046a293fb
Lucas Yule <lyrma@cin.ufpe.br>
2024-02-11 23:35:22 -0300

    refactor(products-service): adding slugify method to createProduct service + adding service params

--- apps/purchases/src/http/graphql/resolvers/products/products.resolver.ts
+++ apps/purchases/src/http/graphql/resolvers/products/products.resolver.ts
@@ -1,5 +1,6 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { ProductsService } from 'src/services/products/products.service';
+import { CreateProductInput } from '../../inputs/create-product-input';
 import { Product } from '../../models/product';
 
 @Resolver()
@@ -11,5 +12,13 @@ export class ProductsResolverResolver {
   // @UseGuards(AuthorizationGuard)
   products() {
     return this.productsService.listAllProducts();
+  }
+
+  @Mutation(() => Product)
+  // * This is a protected endpoint
+  // @UseGuards(AuthorizationGuard)
+  createProduct(@Args('data') data: CreateProductInput) {
+    const { title } = data;
+    return this.productsService.createProduct({ title });
   }
 }
--- apps/purchases/src/services/products/products.service.ts
+++ apps/purchases/src/services/products/products.service.ts
@@ -1,5 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import slugify from 'slugify';
 import { PrismaService } from 'src/database/prisma/prisma.service';
+
+interface CreateProductParams {
+  title: string;
+}
 
 @Injectable()
 export class ProductsService {
@@ -7,5 +12,26 @@ export class ProductsService {
 
   async listAllProducts() {
     return await this.prisma.product.findMany();
+  }
+
+  async createProduct({ title }: CreateProductParams) {
+    const slug = slugify(title, {
+      lower: true,
+    });
+
+    const slugAlreadyExists = await this.prisma.product.findUnique({
+      where: { slug },
+    });
+
+    if (slugAlreadyExists) {
+      throw new Error('Another product with same Slug already exists');
+    }
+
+    return await this.prisma.product.create({
+      data: {
+        title,
+        slug,
+      },
+    });
   }
 }

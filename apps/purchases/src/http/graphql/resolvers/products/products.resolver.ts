f4aa4ee8cbecf23ed5d246aa2c2ede8310bc1c93
Lucas Yule <lyrma@cin.ufpe.br>
2024-02-13 18:10:26 -0300

    refactor(products.resolver): enabling remanescents UseGuards previous disabled

--- apps/purchases/src/http/graphql/resolvers/products/products.resolver.ts
+++ apps/purchases/src/http/graphql/resolvers/products/products.resolver.ts
@@ -2,6 +2,8 @@ import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { ProductsService } from 'src/services/products/products.service';
 import { CreateProductInput } from '../../inputs/create-product-input';
 import { Product } from '../../models/product';
+import { UseGuards } from '@nestjs/common';
+import { AuthorizationGuard } from 'src/http/authorization/authorization.guard';
 
 @Resolver(() => Product)
 export class ProductsResolverResolver {
@@ -9,14 +11,13 @@ export class ProductsResolverResolver {
 
   @Query(() => [Product])
   // * This is a public endpoint
-  // @UseGuards(AuthorizationGuard)
   products() {
     return this.productsService.listAllProducts();
   }
 
   @Mutation(() => Product)
   // * This is a protected endpoint
-  // @UseGuards(AuthorizationGuard)
+  @UseGuards(AuthorizationGuard)
   createProduct(@Args('data') data: CreateProductInput) {
     const { title } = data;
     return this.productsService.createProduct({ title });

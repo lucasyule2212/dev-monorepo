bb336bddce8fd535907f8a1aa8b0171cf53e3c9e
Lucas Yule <lyrma@cin.ufpe.br>
2024-02-14 16:09:49 -0300

    refactor(classroom-http.module): adding services and resolvers to http.module providers

--- apps/classroom/src/http/http.module.ts
+++ apps/classroom/src/http/http.module.ts
@@ -2,8 +2,14 @@ import { ApolloDriver } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
-import path from 'node:path';
+import * as path from 'node:path';
 import { DatabaseModule } from 'src/database/database.module';
+import { CoursesService } from 'src/services/courses.service';
+import { EnrollmentsService } from 'src/services/enrollments.service';
+import { StudentsService } from 'src/services/students.service';
+import { CoursesResolver } from './graphql/resolvers/courses.resolver';
+import { EnrollmentsResolver } from './graphql/resolvers/enrollments.resolver';
+import { StudentsResolver } from './graphql/resolvers/students.resolver';
 
 @Module({
   imports: [
@@ -13,6 +19,14 @@ import { DatabaseModule } from 'src/database/database.module';
       driver: ApolloDriver,
       autoSchemaFile: path.resolve(process.cwd(), 'src/schema.gql'),
     }),
+  ],
+  providers: [
+    CoursesResolver,
+    EnrollmentsResolver,
+    StudentsResolver,
+    CoursesService,
+    EnrollmentsService,
+    StudentsService,
   ],
 })
 export class HttpModule {}

79af6e065fdbcb72adfc26d9ee954e162fbbaafa
Lucas Yule <lyrma@cin.ufpe.br>
2024-02-14 11:27:17 -0300

    feat(customers.resolver): adding customers.resolver + graphql model

---
+++ apps/purchases/src/http/graphql/resolvers/customers/customers.resolver.ts
@@ -0,0 +1,24 @@
+import { Parent, Query, ResolveField, Resolver } from '@nestjs/graphql';
+import { AuthUser, CurrentUser } from 'src/http/authorization/current-user';
+import { CustomersService } from 'src/services/customers.service';
+import { PurchasesService } from 'src/services/purchases/purchases.service';
+import { Customer } from '../../models/customer';
+
+@Resolver(() => Customer)
+export class CustomersResolver {
+  constructor(
+    private customersService: CustomersService,
+    private purchasesService: PurchasesService,
+  ) {}
+
+  @Query(() => Customer)
+  // * This is a public endpoint
+  me(@CurrentUser() user: AuthUser) {
+    return this.customersService.getCustomerByAuthUserId(user.sub);
+  }
+
+  @ResolveField()
+  purchases(@Parent() customer: Customer) {
+    return this.purchasesService.listAllPurchasesFromCustomer(customer.id);
+  }
+}

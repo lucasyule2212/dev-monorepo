4b3fc169144df6ad539a4cb0bc8e6c8eddc41d13
Lucas Yule <lyrma@cin.ufpe.br>
2024-02-14 20:09:02 -0300

    refactor(classroom-resolvers): adding resolvers methods to courses + enrollments + students

--- apps/classroom/src/http/graphql/resolvers/courses.resolver.ts
+++ apps/classroom/src/http/graphql/resolvers/courses.resolver.ts
@@ -1,5 +1,16 @@
-import { Resolver } from '@nestjs/graphql';
+import { Query, Resolver } from '@nestjs/graphql';
 import { Course } from '../models/course';
+import { CoursesService } from 'src/services/courses.service';
+import { UseGuards } from '@nestjs/common';
+import { AuthorizationGuard } from 'src/http/authorization/authorization.guard';
 
 @Resolver(() => Course)
-export class CoursesResolver {}
+export class CoursesResolver {
+  constructor(private coursesService: CoursesService) {}
+
+  @Query(() => [Course])
+  @UseGuards(AuthorizationGuard)
+  courses() {
+    return this.coursesService.listAllCourses();
+  }
+}
--- apps/classroom/src/http/graphql/resolvers/students.resolver.ts
+++ apps/classroom/src/http/graphql/resolvers/students.resolver.ts
@@ -1,16 +1,25 @@
 import { UseGuards } from '@nestjs/common';
-import { Query, Resolver } from '@nestjs/graphql';
+import { Parent, Query, ResolveField, Resolver } from '@nestjs/graphql';
 import { AuthorizationGuard } from 'src/http/authorization/authorization.guard';
+import { EnrollmentsService } from 'src/services/enrollments.service';
 import { StudentsService } from 'src/services/students.service';
 import { Student } from '../models/student';
 
 @Resolver(() => Student)
 export class StudentsResolver {
-  constructor(private studentsService: StudentsService) {}
+  constructor(
+    private studentsService: StudentsService,
+    private enrollmentsService: EnrollmentsService,
+  ) {}
 
   @Query(() => [Student])
   @UseGuards(AuthorizationGuard)
   students() {
     return this.studentsService.listAllStudents();
+  }
+
+  @ResolveField()
+  enrollments(@Parent() student: Student) {
+    return this.enrollmentsService.listEnrollmentsByStudent(student.id);
   }
 }

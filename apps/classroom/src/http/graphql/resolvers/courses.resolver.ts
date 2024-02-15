799aef1b401a7bec609159b676640a745c943554
Lucas Yule <lyrma@cin.ufpe.br>
2024-02-14 22:06:43 -0300

    feat(courses.service): adding createCourse service method and resolver query

--- apps/classroom/src/http/graphql/resolvers/courses.resolver.ts
+++ apps/classroom/src/http/graphql/resolvers/courses.resolver.ts
@@ -1,8 +1,9 @@
-import { Query, Resolver } from '@nestjs/graphql';
-import { Course } from '../models/course';
-import { CoursesService } from 'src/services/courses.service';
 import { UseGuards } from '@nestjs/common';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { AuthorizationGuard } from 'src/http/authorization/authorization.guard';
+import { CoursesService } from 'src/services/courses.service';
+import { CreateCourseInput } from '../inputs/create-course-input';
+import { Course } from '../models/course';
 
 @Resolver(() => Course)
 export class CoursesResolver {
@@ -12,5 +13,11 @@ export class CoursesResolver {
   @UseGuards(AuthorizationGuard)
   courses() {
     return this.coursesService.listAllCourses();
+  }
+
+  @Mutation(() => Course)
+  @UseGuards(AuthorizationGuard)
+  createCourse(@Args('data') data: CreateCourseInput) {
+    return this.coursesService.createCourse(data);
   }
 }
--- apps/classroom/src/services/courses.service.ts
+++ apps/classroom/src/services/courses.service.ts
@@ -1,5 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import slugify from 'slugify';
 import { PrismaService } from 'src/database/prisma/prisma.service';
+
+export interface CreateCourseParams {
+  title: string;
+}
 
 @Injectable()
 export class CoursesService {
@@ -17,6 +22,25 @@ export class CoursesService {
     return await this.prisma.course.findUnique({
       where: {
         id: courseId,
+      },
+    });
+  }
+
+  async createCourse({ title }: CreateCourseParams) {
+    const slug = slugify(title, { lower: true });
+
+    const courseAlreadyExists = await this.prisma.course.findUnique({
+      where: { slug },
+    });
+
+    if (courseAlreadyExists) {
+      throw new Error('Course already exists');
+    }
+
+    return await this.prisma.course.create({
+      data: {
+        title,
+        slug,
       },
     });
   }

097cc5b1cc37f299255fd66bf2a2229b37ff51ac
Lucas Yule <lyrma@cin.ufpe.br>
2024-02-15 18:03:14 -0300

    refactor(classroom-app.module): instanciating messaging-module in app.module

--- apps/classroom/src/app.module.ts
+++ apps/classroom/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { HttpModule } from './http/http.module';
+import { MessagingModule } from './messaging/messaging.module';
 
 @Module({
-  imports: [DatabaseModule, HttpModule],
+  imports: [DatabaseModule, HttpModule, MessagingModule],
   controllers: [AppController],
   providers: [AppService],
 })

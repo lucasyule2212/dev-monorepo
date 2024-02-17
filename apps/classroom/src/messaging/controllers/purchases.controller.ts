8e1c534e4b9e588819ff481dc8382c84330df61f
Lucas Yule <lyrma@cin.ufpe.br>
2024-02-17 17:06:31 -0300

    refactor(purchases.controller): updating purchases.controller with integration to Kafka messages

--- apps/classroom/src/messaging/controllers/purchases.controller.ts
+++ apps/classroom/src/messaging/controllers/purchases.controller.ts
@@ -1,5 +1,8 @@
 import { Controller } from '@nestjs/common';
 import { EventPattern, Payload } from '@nestjs/microservices';
+import { CoursesService } from 'src/services/courses.service';
+import { EnrollmentsService } from 'src/services/enrollments.service';
+import { StudentsService } from 'src/services/students.service';
 
 interface PurchaseCreatedPayload {
   customer: {
@@ -14,8 +17,36 @@ interface PurchaseCreatedPayload {
 
 @Controller()
 export class PurchasesController {
+  constructor(
+    private studentsService: StudentsService,
+    private coursesService: CoursesService,
+    private enrollmentsService: EnrollmentsService,
+  ) {}
+
   @EventPattern('purchases.new-purchase')
   async purchaseCreated(@Payload() value: PurchaseCreatedPayload) {
-    console.log(value);
+    let student = await this.studentsService.getStudentByAuthUserId(
+      value.customer.authUserId,
+    );
+
+    if (!student) {
+      student = await this.studentsService.createStudent({
+        authUserId: value.customer.authUserId,
+      });
+    }
+
+    let course = await this.coursesService.getCourseBySlug(value.product.slug);
+
+    if (!course) {
+      course = await this.coursesService.createCourse({
+        title: value.product.title,
+        slug: value.product.slug,
+      });
+    }
+
+    await this.enrollmentsService.createEnrollment({
+      studentId: student.id,
+      courseId: course.id,
+    });
   }
 }

7d8a496cbe14fd5e495a89fbe7a6572b96f3f7e8
Lucas Yule <lyrma@cin.ufpe.br>
2024-02-18 21:20:16 -0300

    feat(purchases-http.module): adding Apollo Federation configs to http module

--- apps/purchases/src/http/http.module.ts
+++ apps/purchases/src/http/http.module.ts
@@ -1,24 +1,27 @@
-import { ApolloDriver } from '@nestjs/apollo';
+import {
+  ApolloFederationDriver,
+  ApolloFederationDriverConfig,
+} from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import * as path from 'node:path';
 import { DatabaseModule } from 'src/database/database.module';
+import { MessagingModule } from 'src/messaging/messaging.module';
 import { CustomersService } from 'src/services/customers.service';
 import { ProductsService } from 'src/services/products/products.service';
 import { PurchasesService } from 'src/services/purchases/purchases.service';
 import { CustomersResolver } from './graphql/resolvers/customers/customers.resolver';
 import { ProductsResolverResolver } from './graphql/resolvers/products/products.resolver';
 import { PurchasesResolver } from './graphql/resolvers/purchases/purchases.resolver';
-import { MessagingModule } from 'src/messaging/messaging.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     DatabaseModule,
     MessagingModule,
-    GraphQLModule.forRoot({
-      driver: ApolloDriver,
+    GraphQLModule.forRoot<ApolloFederationDriverConfig>({
+      driver: ApolloFederationDriver,
       autoSchemaFile: path.resolve(process.cwd(), 'src/schema.gql'),
     }),
   ],

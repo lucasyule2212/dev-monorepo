93771d477ef3592868cab696619cc382d14e5387
Lucas Yule <lyrma@cin.ufpe.br>
2024-02-19 23:47:04 -0300

    feat(web-apollo-client): adding base apolloClient config + wrapping to ClientProvider

--- apps/web/src/providers/client-provider.tsx
+++ apps/web/src/providers/client-provider.tsx
@@ -1,8 +1,18 @@
 "use client";
+import { apolloClient } from "@/lib/apollo-client";
 import buildProvidersTree from "@/providers/buildProvidersTree";
+import { ApolloProvider } from "@apollo/client";
 import { UserProvider } from "@auth0/nextjs-auth0/client";
 
-const ProvidersTree = buildProvidersTree([[UserProvider, {}]]);
+const ProvidersTree = buildProvidersTree([
+  [UserProvider, {}],
+  [
+    ApolloProvider,
+    {
+      client: apolloClient,
+    },
+  ],
+]);
 
 export default function ClientProvider({
   children,

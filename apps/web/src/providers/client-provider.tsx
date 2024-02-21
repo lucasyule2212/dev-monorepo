37e77c376de8a06816ccbca0d96b1fb422e5ceed
Lucas Yule <lyrma@cin.ufpe.br>
2024-02-20 23:16:09 -0300

    refactor(client-provider): removing obsolete ApolloProvider from client-provider

--- apps/web/src/providers/client-provider.tsx
+++ apps/web/src/providers/client-provider.tsx
@@ -1,18 +1,8 @@
 "use client";
-import { apolloClient } from "@/lib/apollo-client";
 import buildProvidersTree from "@/providers/buildProvidersTree";
-import { ApolloProvider } from "@apollo/client";
 import { UserProvider } from "@auth0/nextjs-auth0/client";
 
-const ProvidersTree = buildProvidersTree([
-  [UserProvider, {}],
-  [
-    ApolloProvider,
-    {
-      client: apolloClient,
-    },
-  ],
-]);
+const ProvidersTree = buildProvidersTree([[UserProvider, {}]]);
 
 export default function ClientProvider({
   children,

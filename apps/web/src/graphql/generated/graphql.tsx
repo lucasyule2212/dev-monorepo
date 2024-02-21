5ae46b54f2c6e32a42062695883151738db1fe70
Lucas Yule <lyrma@cin.ufpe.br>
2024-02-21 09:36:33 -0300

    feat(graphql-queries): adding products.gql query file + generated query hooks

---
+++ apps/web/src/graphql/generated/graphql.tsx
@@ -0,0 +1,202 @@
+import { gql } from "@apollo/client";
+import * as Apollo from "@apollo/client";
+export type Maybe<T> = T | null;
+export type InputMaybe<T> = Maybe<T>;
+export type Exact<T extends { [key: string]: unknown }> = {
+  [K in keyof T]: T[K];
+};
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & {
+  [SubKey in K]?: Maybe<T[SubKey]>;
+};
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & {
+  [SubKey in K]: Maybe<T[SubKey]>;
+};
+export type MakeEmpty<
+  T extends { [key: string]: unknown },
+  K extends keyof T,
+> = { [_ in K]?: never };
+export type Incremental<T> =
+  | T
+  | {
+      [P in keyof T]?: P extends " $fragmentName" | "__typename" ? T[P] : never;
+    };
+const defaultOptions = {} as const;
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: { input: string; output: string };
+  String: { input: string; output: string };
+  Boolean: { input: boolean; output: boolean };
+  Int: { input: number; output: number };
+  Float: { input: number; output: number };
+  DateTime: { input: any; output: any };
+};
+
+export type Course = {
+  __typename?: "Course";
+  id: Scalars["ID"]["output"];
+  slug: Scalars["String"]["output"];
+  title: Scalars["String"]["output"];
+};
+
+export type CreateCourseInput = {
+  title: Scalars["String"]["input"];
+};
+
+export type CreateProductInput = {
+  title: Scalars["String"]["input"];
+};
+
+export type CreatePurchaseInput = {
+  productId: Scalars["String"]["input"];
+};
+
+export type Enrollment = {
+  __typename?: "Enrollment";
+  canceledAt?: Maybe<Scalars["DateTime"]["output"]>;
+  course: Course;
+  createdAt?: Maybe<Scalars["DateTime"]["output"]>;
+  id: Scalars["ID"]["output"];
+  student: User;
+};
+
+export type Mutation = {
+  __typename?: "Mutation";
+  createCourse: Course;
+  createProduct: Product;
+  createPurchase: Purchase;
+};
+
+export type MutationCreateCourseArgs = {
+  data: CreateCourseInput;
+};
+
+export type MutationCreateProductArgs = {
+  data: CreateProductInput;
+};
+
+export type MutationCreatePurchaseArgs = {
+  data: CreatePurchaseInput;
+};
+
+export type Product = {
+  __typename?: "Product";
+  id: Scalars["ID"]["output"];
+  slug: Scalars["String"]["output"];
+  title: Scalars["String"]["output"];
+};
+
+export type Purchase = {
+  __typename?: "Purchase";
+  createdAt: Scalars["DateTime"]["output"];
+  id: Scalars["ID"]["output"];
+  product: Product;
+  status: PurchaseStatus;
+};
+
+/** Status of the purchase */
+export enum PurchaseStatus {
+  Approved = "APPROVED",
+  Failed = "FAILED",
+  Pending = "PENDING",
+}
+
+export type Query = {
+  __typename?: "Query";
+  course: Course;
+  courses: Array<Course>;
+  enrollments: Array<Enrollment>;
+  me: User;
+  products: Array<Product>;
+  purchases: Array<Purchase>;
+  students: Array<User>;
+};
+
+export type QueryCourseArgs = {
+  id: Scalars["String"]["input"];
+};
+
+export type User = {
+  __typename?: "User";
+  authUserId: Scalars["ID"]["output"];
+  enrollment: Array<Enrollment>;
+  purchases: Array<Purchase>;
+};
+
+export type GetProductsQueryVariables = Exact<{ [key: string]: never }>;
+
+export type GetProductsQuery = {
+  __typename?: "Query";
+  products: Array<{ __typename?: "Product"; id: string; title: string }>;
+};
+
+export const GetProductsDocument = gql`
+  query GetProducts {
+    products {
+      id
+      title
+    }
+  }
+`;
+
+/**
+ * __useGetProductsQuery__
+ *
+ * To run a query within a React component, call `useGetProductsQuery` and pass it any options that fit your needs.
+ * When your component renders, `useGetProductsQuery` returns an object from Apollo Client that contains loading, error, and data properties
+ * you can use to render your UI.
+ *
+ * @param baseOptions options that will be passed into the query, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options;
+ *
+ * @example
+ * const { data, loading, error } = useGetProductsQuery({
+ *   variables: {
+ *   },
+ * });
+ */
+export function useGetProductsQuery(
+  baseOptions?: Apollo.QueryHookOptions<
+    GetProductsQuery,
+    GetProductsQueryVariables
+  >,
+) {
+  const options = { ...defaultOptions, ...baseOptions };
+  return Apollo.useQuery<GetProductsQuery, GetProductsQueryVariables>(
+    GetProductsDocument,
+    options,
+  );
+}
+export function useGetProductsLazyQuery(
+  baseOptions?: Apollo.LazyQueryHookOptions<
+    GetProductsQuery,
+    GetProductsQueryVariables
+  >,
+) {
+  const options = { ...defaultOptions, ...baseOptions };
+  return Apollo.useLazyQuery<GetProductsQuery, GetProductsQueryVariables>(
+    GetProductsDocument,
+    options,
+  );
+}
+export function useGetProductsSuspenseQuery(
+  baseOptions?: Apollo.SuspenseQueryHookOptions<
+    GetProductsQuery,
+    GetProductsQueryVariables
+  >,
+) {
+  const options = { ...defaultOptions, ...baseOptions };
+  return Apollo.useSuspenseQuery<GetProductsQuery, GetProductsQueryVariables>(
+    GetProductsDocument,
+    options,
+  );
+}
+export type GetProductsQueryHookResult = ReturnType<typeof useGetProductsQuery>;
+export type GetProductsLazyQueryHookResult = ReturnType<
+  typeof useGetProductsLazyQuery
+>;
+export type GetProductsSuspenseQueryHookResult = ReturnType<
+  typeof useGetProductsSuspenseQuery
+>;
+export type GetProductsQueryResult = Apollo.QueryResult<
+  GetProductsQuery,
+  GetProductsQueryVariables
+>;

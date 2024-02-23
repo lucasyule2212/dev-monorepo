31e1569a05b81934d635184e0467c4760835fce4
Lucas Yule <lyrma@cin.ufpe.br>
2024-02-23 14:17:11 -0300

    refactor(next.config.js): adding image loaders config at next.config

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   transpilePackages: ["@repo/ui"],
+  images: {
+    remotePatterns: [
+      {
+        hostname: "lh3.googleusercontent.com",
+      },
+      {
+        hostname: "images.unsplash.com",
+      },
+    ],
+  },
 };

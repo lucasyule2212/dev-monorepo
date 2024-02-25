c0211d2e6e434502e46257bdab9960b3634bceaa
Lucas Yule <lyrma@cin.ufpe.br>
2024-02-24 21:40:58 -0300

    refactor(web-layout): adding toaster provider

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { Footer } from "@/components/Footer";
 import { Header } from "@/components/Header";
 import ClientProvider from "@/providers/client-provider";
+import { Toaster } from "@repo/ui/components/ui/sonner";
 import "@repo/ui/globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
@@ -20,6 +21,7 @@ export default function RootLayout({
     <html lang="en">
       <ClientProvider>
         <body className={`${inter.className} bg-slate-950 relative`}>
+          <Toaster />
           <Header />
           {children}
           <Footer />

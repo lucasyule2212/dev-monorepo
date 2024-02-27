96c0d69d6dd6b196bdcfca514207a43b574b26ab
Lucas Yule <lyrma@cin.ufpe.br>
2024-02-27 13:56:38 -0300

    refactor(header): adding available courses link

--- apps/web/src/components/Header.tsx
+++ apps/web/src/components/Header.tsx
@@ -24,6 +24,13 @@ export function Header() {
           {user ? (
             <>
               <Link
+                key="courses"
+                href="/home"
+                className="flex items-center gap-2 text-xs sm:text-sm font-medium text-gray-300 hover:text-white transition-colors bg-slate-950 rounded-md px-3 py-2 hover:bg-slate-800 ring-1 ring-slate-800"
+              >
+                Available Courses
+              </Link>
+              <Link
                 key="my-courses"
                 href="/my-courses"
                 className="flex items-center gap-2 text-xs sm:text-sm font-medium text-gray-300 hover:text-white transition-colors bg-slate-950 rounded-md px-3 py-2 hover:bg-slate-800 ring-1 ring-slate-800"

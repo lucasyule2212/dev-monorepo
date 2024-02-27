76af6d0fad637c036bd18b6b09c7e6ed9cdf9fee
Lucas Yule <lyrma@cin.ufpe.br>
2024-02-27 13:13:37 -0300

    refactor(apolloprovider): removing obsolete imports at apolloProvider

--- apps/web/src/lib/apolloProvider.tsx
+++ apps/web/src/lib/apolloProvider.tsx
@@ -1,7 +1,5 @@
 "use client";
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
-import { getAccessToken } from "@auth0/nextjs-auth0";
-import { useEffect, useState } from "react";
 
 export const ApolloClientProvider = ({
   children,
